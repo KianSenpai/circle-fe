24efd47c3951f2882c2d69f8d65cc143b4bd4bb1
Kian AliAkbari <kiana@kaytu.io>
2024-03-16 01:06:25 +0400

    feat: added base pages

--- src/utilities/router.ts
+++ src/utilities/router.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HelloWorld from '../components/HelloWorld.vue'
+import Home from '../views/Home.vue'
 
 const routes = [
     {
         path: '/',
         name: 'home',
-        component: HelloWorld,
+        component: Home,
     },
 ]
 
