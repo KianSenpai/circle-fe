308ea81c74c080c6acaeafbbdf7dccdd372f8332
Kian AliAkbari <kiana@kaytu.io>
2024-03-17 12:15:58 +0400

    feat: detail page

---
+++ src/components/CostumeButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+    <button
+        class="w-fit rounded-md border border-blue-500 px-2 py-1 text-blue-500 transition-all hover:bg-blue-500 hover:text-white"
+    >
+        <slot />
+    </button>
+</template>
+
+<style scoped></style>
--- src/views/BookDetail.vue
+++ src/views/BookDetail.vue
@@ -2,17 +2,19 @@
 import { onMounted, ref } from 'vue'
 import { getBookDetail, IBookDetail } from '../utilities/API.ts'
 import Loading from '../components/Loading.vue'
+import CostumeButton from '../components/CostumeButton.vue'
 
-let data = ref<IBookDetail | undefined>(undefined)
 const props = defineProps({
     id: {
         type: Number,
         required: true,
     },
 })
+
+let data = ref<IBookDetail | undefined>(undefined)
+
 onMounted(async () => {
     data.value = await getBookDetail(props.id)
-    console.log(data.value)
 })
 </script>
 
@@ -23,15 +25,46 @@ onMounted(async () => {
             <span> > </span>
             <span>{{ data ? data.book.title : 'an amazing book!' }}</span>
         </div>
-        <div class="flex flex-col gap-8 md:flex-row">
+        <div class="flex flex-col gap-4 md:flex-row md:gap-8">
             <img
                 src="../assets/book.svg"
                 class="w-full rounded-md border py-6 md:w-1/3"
                 :alt="data ? data.book.title : 'circle challenge'"
             />
-            <div class="flex flex-col">
-                <div class="flex flex-col">
-                    <span>{{}}</span>
+            <div class="flex w-full flex-col py-6">
+                <div class="flex w-full flex-col gap-1 border-b pb-4">
+                    <span class="text-xl font-semibold">{{
+                        data.book.title
+                    }}</span>
+                    <span>{{ data.book.author }}</span>
+                </div>
+                <div class="flex h-full w-full flex-col justify-between pt-4">
+                    <div class="flex items-center gap-1">
+                        <span class="text-gray-500">ISBN:</span>
+                        <a
+                            class="transition-all hover:text-blue-500"
+                            :href="`https://isbnsearch.org/isbn/${data.book.isbn}`"
+                        >
+                            {{ data.book.isbn }}</a
+                        >
+                    </div>
+                    <div class="flex w-full flex-col gap-2">
+                        <div class="flex items-center gap-1">
+                            <span class="text-gray-500">Price:</span>
+                            <span class="font-semibold"
+                                >${{ data.book.price }}</span
+                            >
+                        </div>
+                        <div class="flex w-full justify-between">
+                            <costume-button>Add to cart</costume-button>
+                            <div class="flex items-center gap-1">
+                                <span class="text-gray-500"
+                                    >Available Stock:</span
+                                >
+                                <span>{{ data.book.availableStock }}</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
