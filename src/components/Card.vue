488d38cce0e09795d25b5217255f2c6b869be234
Kian AliAkbari <kiana@kaytu.io>
2024-03-17 10:59:38 +0400

    fix: card fix

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -7,12 +7,22 @@ defineProps<{ book: IBook }>()
 <template>
     <router-link
         :to="`/book/${book.id}`"
-        class="flex cursor-pointer flex-col rounded-md border bg-gray-50 p-4 shadow transition-all hover:shadow-md"
+        class="flex cursor-pointer gap-4 rounded-md border bg-gray-50 p-4 shadow transition-all hover:shadow-md"
     >
-        <span class="text font-semibold">{{ book.title }}</span>
-        <span>{{ book.author }}</span>
-        <span>${{ book.price }}</span>
-        <span class="text-xs text-gray-500">ISBN: {{ book.isbn }}</span>
+        <img src="../assets/book.svg" width="100" :alt="book.title" />
+        <div class="flex h-full w-full flex-col justify-between">
+            <div class="flex flex-col">
+                <span class="text font-semibold">{{ book.title }}</span>
+                <span>{{ book.author }}</span>
+            </div>
+            <div class="flex w-full items-center justify-between">
+                <span>${{ book.price }}</span>
+                <span class="text-xs text-gray-500"
+                    >Available Stock: {{ book.availableStock }}</span
+                >
+            </div>
+            <!--            <span class="text-xs text-gray-500">ISBN: {{ book.isbn }}</span>-->
+        </div>
     </router-link>
 </template>
 
